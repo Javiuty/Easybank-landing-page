d3f17e26f110db5958cc47c5b83759f86b27f812
Javiuty <javiiescuadra@gmail.com>
2021-02-14 15:41:26 +0100

    adding burger menu funcionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", () => {
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
